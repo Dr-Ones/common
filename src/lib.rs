a5ee743fac520bc17237417489e9a61d1c6cb86b
W. F. <we.f@posteo.net>
2025-01-07 10:19:25 +0100

    Remove redundant test functions from client and server, prepare logging update

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,8 @@
 //! This crate provides shared functionality used by the drone, client,
 //! and server components of the network simulator.
 
+mod logging;
 mod network_node;
+
+pub use logging::*;
 pub use network_node::*;
---
+++ src/logging.rs
@@ -0,0 +1,47 @@
+use once_cell::sync::Lazy;
+use std::sync::Mutex;
+
+static LOGGING_ENABLED: Lazy<Mutex<bool>> = Lazy::new(|| Mutex::new(true));
+
+pub fn enable_logging() {
+    *LOGGING_ENABLED.lock().unwrap() = true;
+}
+
+pub fn disable_logging() {
+    *LOGGING_ENABLED.lock().unwrap() = false;
+}
+
+pub fn is_logging_enabled() -> bool {
+    *LOGGING_ENABLED.lock().unwrap()
+}
+
+#[macro_export]
+macro_rules! log_status {
+    ($node_id:expr, $($arg:tt)*) => {
+        if $crate::logging::is_logging_enabled() {
+            println!("[NODE {}] {}", $node_id, format!($($arg)*));
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! log_error {
+    ($node_id:expr, $($arg:tt)*) => {
+        if $crate::logging::is_logging_enabled() {
+            eprintln!("[NODE {}] Error: {}", $node_id, format!($($arg)*));
+        }
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_enable_disable() {
+        enable_logging();
+        assert!(is_logging_enabled());
+        disable_logging();
+        assert!(!is_logging_enabled());
+    }
+}
